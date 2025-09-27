c1048621e6ef6f7b4e09cef0c99076c70d53fe3b
BriceHarper <58884670+BriceHarper@users.noreply.github.com>
2025-09-27 19:58:01 +0200

    feat: Add some strategies to check opened TCP/UDP ports

---
+++ PortScanner/strategies.py
@@ -0,0 +1,28 @@
+import socket
+
+class ScannerStrategy:
+        def scan(self, host, port):
+            try:
+                return self._scan(host, port)
+            except socket.gaierror:
+                print("Adresse incorrecte.")
+                return False
+            except TimeoutError:
+                print("Le scan a pris trop de temps.")
+                return False
+            except Exception as e:
+                print("Problème : " + e)
+        
+        def scan(self, host, port):
+            raise NotImplementedError()
+        
+class TCPScannerStrategy(ScannerStrategy):
+      def scan(self, host, port) ->bool:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                return s.connect_ex((host, port)) == 0
+
+class UDPScannerStrategy(ScannerStrategy):
+      def scan(self, host, port) ->bool:
+            with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+                return s.connect_ex((host, port)) == 0
+            
