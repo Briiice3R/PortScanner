b56779929e34d9f79f718452445e7e2be3624982
BriceHarper <58884670+BriceHarper@users.noreply.github.com>
2025-10-08 09:02:39 +0200

    refactor: Give scan result to render_template

--- web/src/main.py
+++ web/src/main.py
@@ -1,17 +1,24 @@
 # from PortScanner import PortScanner
 # from PortScanner.strategies import TCPScannerStrategy
 from flask import Flask, render_template, request
-
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
+from PortScanner.scan import PortScanner
+from PortScanner.strategies import TCPScannerStrategy, UDPScannerStrategy
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def launch_scan():
-    protocol = request.args.get("protocol")
-    if protocol is None:
+    protocol = request.args.get("protocol") or "TCP"
+    host = request.args.get("host") or None
+    if protocol is None or host is None:
         return render_template("index.html")
     else:
-        print(protocol)
-        return render_template("index.html")
+        print("Ici")
+        scanner = PortScanner(host, TCPScannerStrategy() if protocol=="TCP" else UDPScannerStrategy())
+        scanner.running_scan()
+        return render_template("index.html", res=scanner.result)
 
 
 
